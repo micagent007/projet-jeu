8e5db3c762b2f23e68a4cd0a6d069d43243a3b92
mickael laporte <micah.laporte@aol.com>
2022-05-18 16:30:37 +0200

    obstacle move

--- obstacle.cpp
+++ obstacle.cpp
@@ -4,7 +4,7 @@
 obstacle::obstacle(int n){
     if (n==1){
         for (int k=0;k<=3*w;k++){
-            cord O={k,k+hFloor};
+            cord O={k+xDino+wDino,hFloor-k};
             P.push_back(O);
             cord_double T={1,1};
             D.push_back(T);
--- recorder.cpp
+++ recorder.cpp
@@ -14,10 +14,11 @@ void Recorder::display_obstacle(int to, obstacle O){
     clearWindow();
     fillRect(xDino,hFloor-T.altitude(to),wDino,-hDino,GREEN);
     int n=O.P.size();
-    for(int i=0;i<=n-w+1;i++){
-            drawRect(O.P[i].x-T.jumptime(to)%w,O.P[i].y-T.jumptime(to)%w,1,1,RED);
+    for(int i=0;i<=n;i++){
+            drawRect(O.P[i].x-T.jumptime(to)%w,O.P[i].y,1,1,RED);
 
     }
+    T.jumpset(T.jumptime(to)+1,to);
 
 }
 
